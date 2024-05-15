a578e7e4ca6595295bbade160ed53a44cc5af723
Deo Farady S <deofarady43@gmail.com>
2024-05-15 17:58:52 +0700

    update login & register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,87 +1,129 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Login</title>
-    <link href="https://fonts.googleapis.com/css?family=Karla:400,700&amp;display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="css/materialdesignicons.min.css">
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/login.css">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Travesia | Login</title>
+
+    <!-- link bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
 <body>
-    <main class="d-flex align-items-center min-vh-100 py-3 py-md-0">
-        <div class="container">
-            <div class="card login-card">
-                <div class="row no-gutters">
-                    <div class="col-md-5">
-                        <img src="images/img_login.png" alt="login" class="login-card-img">
-                    </div>
-                    <div class="col-md-7">
-                        <div class="card-body">
-                            <p class="login-card-description">Selamat Datang Kembali</p>
-                            <p class="login-card-description">Daftar Sekarang</p>
-                            <p><small class="text-muted">Daftarkan akunmu dengan cepat dan mudah.</small></p>
-                            <form action="{{ route('login.post') }}" method="POST" id="loginForm">
-                                @csrf
-                                <div class="form-group">
-                                    <label for="email" class="sr-only">Email</label>
-                                    <input type="email" name="email" id="email" class="form-control"
-                                        placeholder="Email address">
-                                </div>
-                                <div class="form-group mb-4">
-                                    <label for="password" class="sr-only">Password</label>
-                                    <input type="password" name="password" id="password" class="form-control"
-                                        placeholder="Kata Sandi">
-                                </div>
-                                <button type="submit" class="btn btn-block login-btn mb-4">Login</button>
-                            </form>
-                            <div class="d-flex align-items-center">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" id="remember-me-checkbox">
-                                    <label class="form-check-label" for="remember-me-checkbox">Remember me</label>
-                                </div>
-                                <div style="margin-left: 16%;">
-                                    <a href="#!" class="forgot-password-link">Forgot password?</a>
-                                </div>
-                            </div>
-                            <br>
-                            <p class="login-card-footer-text">Don't have an account? <a href="{{ route('register') }}"
-                                    class="text-reset">Register here</a>
-                            </p>
+    <style>
+        :root {
+            --accent: #0B8CE8;
+            --black: #000;
+            --white: #fff;
 
-                        </div>
-                    </div>
-                </div>
-            </div>
+            --button: 500 16px Inter;
+            --body-1: 400 16px Inter;
+            --body-2: 400 14px Inter;
+        }
+
+        body {
+            font-size: 16px;
+
+            .row {
+                height: fit-content;
+
+                .col {
+                    width: 50%;
+                    padding: 0;
+                    z-index: 1;
+                    height: 100vh;
+
+                    img {
+                        top: -50%;
+                        width: 105%;
+                        height: 100%;
+                        z-index: 0;
+                    }
+                }
+
+                .col-4 {
+                    background-color: var(--white);
+                    padding: 0;
+                    z-index: 1;
+                    width: 50%;
+                    gap: 32px;
+                    height: 100vh;
+                    padding: 100px;
+                    border-radius: 36px 0 0 36px;
+                    box-shadow: 0 7px 30px 0 rgba(0, 0, 0, 0.2);
+
+
+                    #back{
+                        width: fit-content;
+                        padding: 4px;
+                        border-radius: 10px;
+                        align-items: center;
+                    }
+                    #back:hover{
+                        background-color: #e8ecef;
+                    }
+                    form {
+                        width: auto;
+
+                        .mb-3 {
+
+                            input {
+                                width: 100%;
+                                padding: 12px;
+                            }
+                        }
+
+                        button {
+                            width: 100%;
+                            padding: 12px;
+                            font: var(--button);
+                        }
+                    }
+
+                    p {
+                        width: auto;
+                    }
+                }
+            }
+        }
+    </style>
+
+
+
+    <div class="row">
+
+        <div class="col">
+            <img src="/assets/image/bg.png" alt="">
         </div>
-    </main>
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    @if ($errors->any())
-        <script>
-            Swal.fire({
-                icon: 'error',
-                title: 'Oops...',
-                text: '{{ $errors->first() }}'
-            });
-        </script>
-    @elseif(session('success'))
-        <script>
-            Swal.fire({
-                icon: 'success',
-                title: 'Success!',
-                text: '{{ session('success') }}'
-            });
-        </script>
-    @endif
 
-    <script src="js/jquery-3.4.1.min.js"></script>
-    <script src="js/popper.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
+        <div class="col-4 d-flex flex-column">
+            <a href="/" class="text-decoration-none d-flex" id="back">
+                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#212529bf">
+                    <path d="m313-440 196 196q12 12 11.5 28T508-188q-12 11-28 11.5T452-188L188-452q-6-6-8.5-13t-2.5-15q0-8 2.5-15t8.5-13l264-264q11-11 27.5-11t28.5 11q12 12 12 28.5T508-715L313-520h447q17 0 28.5 11.5T800-480q0 17-11.5 28.5T760-440H313Z" />
+                </svg>
+            </a>
 
+            <h1>Sign In</h1>
+            <form>
+                <div class="mb-3">
+                    <input type="text" class="form-control" id="username" placeholder="Username">
+                </div>
+                <div class="mb-3">
+                    <input type="password" class="form-control" id="password" placeholder="Password">
+                </div>
+                <button type="submit" class="btn btn-primary flex-grow-1">Sign In</button>
+            </form>
+            <p class="text-secondary text-center">By logging in you agree to Travesia's <span class="text-primary">Terms & Conditions</span> and <span class="text-primary">Privacy Policy</span> Travesia.</p>
+            <p class="text-center mt-5">Don't have an account? <a href="/sign-up" class="text-decoration-none text-center">Sign Up</a></p>
+        </div>
+    </div>
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 
 </html>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,125 +1,130 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Register</title>
-    <link href="https://fonts.googleapis.com/css?family=Karla:400,700&amp;display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="css/materialdesignicons.min.css">
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/login.css">
-    <style>
-        .btn-close {
-            position: absolute;
-            top: 8px;
-            right: 8px;
-            font-size: 12px;
-            padding: 4px 8px;
-        }
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Travesia | Login</title>
 
-        .txt-close {
-            position: absolute;
-            top: 12px;
-            right: 60px;
-            font-size: 14px;
-        }
-
-        /* Responsive Styles */
-        @media (max-width: 767px) {
-            .txt-close {
-                top: 12px;
-                right: 60px;
-                font-size: 12px;
-            }
-
-            .btn-close {
-                top: 12 px
-            }
-        }
-    </style>
+    <!-- link bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
 <body>
-    <main class="d-flex align-items-center min-vh-100 py-3 py-md-0">
-        <div class="container">
-            <div class="card login-card">
-                <div class="row no-gutters">
-                    <div class="col-md-5">
-                        <img src="{{ asset('images/2.png') }}" alt="login" class="login-card-img">
-                    </div>
-                    <div class="col-md-7 position-relative">
-                        <div class="card-body">
-                            <p class="txt-close">Sudah punya akun?</p>
-                            <p><button class="btn btn-outline-primary btn-close"
-                                onclick="window.location.href='{{ route('login') }}';">masuk</button>
-                            <p class="login-card-description"><b>Daftarkan Sekarang</b></p>
-                            <p><small class="text-muted">Daftarkan akunmu dengan cepat dan mudah.</small></p>
-                            <form action="{{ route('register.post') }}" method="POST" id="registerForm">
-                                @csrf
-                                <div class="form-group">
-                                    <label for="name" class="sr-only">Name</label>
-                                    <input type="text" name="name" id="name" class="form-control"
-                                        placeholder="Nama" required>
-                                </div>
-                                <div class="form-group">
-                                    <label for="email" class="sr-only">Email</label>
-                                    <input type="email" name="email" id="email" class="form-control"
-                                        placeholder="Email" required>
-                                </div>
-                                <div class="form-group mb-4">
-                                    <label for="password" class="sr-only">Kata Sandi</label>
-                                    <input type="password" name="password" id="password" class="form-control"
-                                        placeholder="Kata Sandi" required>
-                                </div>
-                                <div class="form-group mb-4">
-                                    <label for="password_confirmation" class="sr-only">Konfirmasi Kata Sandi</label>
-                                    <input type="password" name="password_confirmation" id="password_confirmation"
-                                        class="form-control" placeholder="Konfirmasi Kata Sandi" required>
-                                </div>
-                                <button type="submit" class="btn btn-block login-btn mb-4">Daftar</button>
-                            </form>
-                            <br>
-                            <p class="login-card-footer-text">Dengan mendaftar, saya menyetujui
-                                <br>
-                                <a href="#!" class="text-reset" style="color: blue;">Syarat dan Ketentuan serta
-                                    Kebijakan Privasi</a>
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </main>
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    <script>
-        document.getElementById('registerForm').addEventListener('submit', function(event) {
-            var password = document.getElementById('password').value;
-            var confirmPassword = document.getElementById('password_confirmation').value;
+    <style>
+        :root {
+            --accent: #0B8CE8;
+            --black: #000;
+            --white: #fff;
 
-            if (password !== confirmPassword) {
-                event.preventDefault(); // Menghentikan pengiriman formulir
-                Swal.fire({
-                    icon: 'error',
-                    title: 'Oops...',
-                    text: 'Password and Confirm Password do not match.'
-                });
+            --button: 500 16px Inter;
+            --body-1: 400 16px Inter;
+            --body-2: 400 14px Inter;
+        }
+
+        body {
+            font-size: 16px;
+
+            .row {
+                height: fit-content;
+
+                .col {
+                    width: 50%;
+                    padding: 0;
+                    z-index: 1;
+                    height: 100vh;
+
+                    img {
+                        top: -50%;
+                        width: 105%;
+                        height: 100%;
+                        z-index: 0;
+                    }
+                }
+
+                .col-4 {
+                    background-color: var(--white);
+                    padding: 0;
+                    z-index: 1;
+                    width: 50%;
+                    gap: 32px;
+                    height: 100vh;
+                    padding: 100px;
+                    border-radius: 36px 0 0 36px;
+                    box-shadow: 0 7px 30px 0 rgba(0, 0, 0, 0.2);
+
+
+                    #back{
+                        width: fit-content;
+                        padding: 4px;
+                        border-radius: 10px;
+                        align-items: center;
+                    }
+                    #back:hover{
+                        background-color: #e8ecef;
+                    }
+                    form {
+                        width: auto;
+
+                        .mb-3 {
+
+                            input {
+                                width: 100%;
+                                padding: 12px;
+                            }
+                        }
+
+                        button {
+                            width: 100%;
+                            padding: 12px;
+                            font: var(--button);
+                        }
+                    }
+
+                    p {
+                        width: auto;
+                    }
+                }
             }
-        });
-    </script>
-    @if ($errors->any())
-        <script>
-            Swal.fire({
-                icon: 'error',
-                title: 'Oops...',
-                text: '{{ $errors->first() }}'
-            });
-        </script>
-    @endif
-    <script src="js/jquery-3.4.1.min.js"></script>
-    <script src="js/popper.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
+        }
+    </style>
 
+
+
+    <div class="row">
+
+        <div class="col">
+            <img src="/assets/image/bg.png" alt="">
+        </div>
+
+        <div class="col-4 d-flex flex-column">
+            <a href="/" class="text-decoration-none d-flex" id="back">
+                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#212529bf">
+                    <path d="m313-440 196 196q12 12 11.5 28T508-188q-12 11-28 11.5T452-188L188-452q-6-6-8.5-13t-2.5-15q0-8 2.5-15t8.5-13l264-264q11-11 27.5-11t28.5 11q12 12 12 28.5T508-715L313-520h447q17 0 28.5 11.5T800-480q0 17-11.5 28.5T760-440H313Z" />
+                </svg>
+
+            </a>
+
+            <h1>Sign Up</h1>
+            <form>
+                <div class="mb-3">
+                    <input type="text" class="form-control" id="username" placeholder="Username">
+                </div>
+                <div class="mb-3">
+                    <input type="password" class="form-control" id="password" placeholder="Password">
+                </div>
+                <button type="submit" class="btn btn-primary flex-grow-1">Submit</button>
+            </form>
+            <p class="text-secondary text-center">By registering you agree to Travesia's <span class="text-primary">Terms & Conditions</span> and <span class="text-primary">Privacy Policy</span> Travesia.</p>
+            <p class="text-center mt-5">Already have an account? <a href="/sign-in" class="text-decoration-none text-center">Sign In Here</a></p>
+        </div>
+    </div>
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
+
 </html>
